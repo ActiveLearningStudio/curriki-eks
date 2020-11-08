ee1512ade2085524d8ef2c41a7eda66c30618cfe
IMCoder <imcoder001@gmail.com>
2020-11-08 18:30:18 +0500

    storage updates

--- storage/storage/app/public/h5p/h5p-editor/ckeditor/config.js
+++ storage/storage/app/public/h5p/h5p-editor/ckeditor/config.js
@@ -1,45 +1,48 @@
 /**
- * @license Copyright (c) 2003-2018, CKSource - Frederico Knabben. All rights reserved.
+ * @license Copyright (c) 2003-2020, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
  */
 
 CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here.
 	// For complete reference see:
-	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
+	// https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_config.html
 
-	// The toolbar groups arrangement, optimized for a single toolbar row.
+	// The toolbar groups arrangement, optimized for two toolbar rows.
 	config.toolbarGroups = [
-		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
 		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
 		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
-		{ name: 'forms' },
-		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
 		{ name: 'links' },
 		{ name: 'insert' },
+		{ name: 'forms' },
+		{ name: 'tools' },
+		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
+		{ name: 'others' },
+		'/',
+		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
 		{ name: 'styles' },
 		{ name: 'colors' },
-		{ name: 'tools' },
-		{ name: 'others' },
 		{ name: 'about' }
 	];
 
-	// The default plugins included in the basic setup define some buttons that
-	// are not needed in a basic editor. They are removed here.
-	//	config.removeButtons = 'Cut,Copy,Paste,Undo,Redo,Anchor,Underline,Strike,Subscript,Superscript';
+	// Remove some buttons provided by the standard plugins, which are
+	// not needed in the Standard(s) toolbar.
+	// config.removeButtons = 'Underline,Subscript,Superscript';
 
+	// remove unnecessary plugins
+	config.removePlugins = 'cloudservices,easyimage,googledocs';
 
-		// Dialog windows are also simplified.
-		config.removeDialogTabs = 'image:advanced;link:advanced';
+	// extra plugins
+	config.plugins += ',font,colorbutton,justify';
 
-		// Se the most common block elements.
-		config.format_tags = 'p;h1;h2;h3;pre';
+	// Set the most common block elements.
+	config.format_tags = 'p;h1;h2;h3;pre';
 
-		config.autoGrow_minHeight = 80;
-		config.height = 80;
-		config.autoGrow_onStartup = true;
-		config.autoGrow_maxHeight = 500;
-		config.colorButton_enableMore = true;
-		config.plugins += ',removeRedundantNBSP';
+	// Simplify the dialog windows.
+	config.removeDialogTabs = 'image:advanced;link:advanced';
+
+	config.autoGrow_onStartup = true;
+	config.isFormatting = true;
+	config.colorButton_enableMore = true;
 };
